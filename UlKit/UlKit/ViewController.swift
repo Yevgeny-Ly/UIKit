18adfa5306a6782499c52333f6fb07a6ec83e8c3
Yevgeny-Ly <121032402+Yevgeny-Ly@users.noreply.github.com>
2024-02-05 16:01:28 +0300

    init

---
+++ UlKit/UlKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UlKit
+//
+//  Created by Евгений Л on 05.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
