75b0afe79aff0337f026924d03740c39b11debbf
Yevgeny Ly <globchenko16@gmail.com>
2024-02-12 19:57:20 +0300

    - добавлены шрифты для проекта

---
+++ UlKit/UlKit/Extension/UIFont+Extension.swift
@@ -0,0 +1,94 @@
+//
+//  UIFont+Extension.swift
+//  UlKit
+//
+//  Created by Евгений Л on 12.02.2024.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    ///Verdana
+    static func verdana14() -> UIFont? {
+        UIFont(name: "Verdana", size: 14)
+    }
+
+    static func verdana16() -> UIFont? {
+        UIFont(name: "Verdana", size: 16)
+    }
+
+    static func verdana20() -> UIFont? {
+        UIFont(name: "Verdana", size: 20)
+    }
+
+    static func verdana22() -> UIFont? {
+        UIFont(name: "Verdana", size: 22)
+    }
+
+    static func verdana24() -> UIFont? {
+        UIFont(name: "Verdana", size: 24)
+    }
+
+    static func verdana48() -> UIFont? {
+        UIFont(name: "Verdana", size: 48)
+    }
+
+    ///Verdana Bold
+    static func verdanaBold12() -> UIFont? {
+        UIFont(name: "Verdana-Bold", size: 12)
+    }
+
+    static func verdanaBold14() -> UIFont? {
+        UIFont(name: "Verdana-Bold", size: 14)
+    }
+
+    static func verdanaBold16() -> UIFont? {
+        UIFont(name: "Verdana-Bold", size: 16)
+    }
+
+    static func verdanaBold18() -> UIFont? {
+        UIFont(name: "Verdana-Bold", size: 18)
+    }
+
+    static func verdanaBold20() -> UIFont? {
+        UIFont(name: "Verdana-Bold", size: 20)
+    }
+
+    static func verdanaBold22() -> UIFont? {
+        UIFont(name: "Verdana-Bold", size: 22)
+    }
+
+    static func verdanaBold24() -> UIFont? {
+        UIFont(name: "Verdana-Bold", size: 24)
+    }
+
+    static func verdanaBold26() -> UIFont? {
+        UIFont(name: "Verdana-Bold", size: 26)
+    }
+
+    ///Verdana Italic
+    static func verdanaItalic14() -> UIFont? {
+        UIFont(name: "Verdana-Italic", size: 14)
+    }
+
+    static func verdanaItalic16() -> UIFont? {
+        UIFont(name: "Verdana-Italic", size: 16)
+    }
+
+    static func verdanaItalic20() -> UIFont? {
+        UIFont(name: "Verdana-Italic", size: 20)
+    }
+
+    static func verdanaItalic22() -> UIFont? {
+        UIFont(name: "Verdana-Italic", size: 22)
+    }
+
+    static func verdanaItalic24() -> UIFont? {
+        UIFont(name: "Verdana-Italic", size: 24)
+    }
+
+    static func verdanaItalic48() -> UIFont? {
+        UIFont(name: "Verdana-Italic", size: 48)
+    }
+}
