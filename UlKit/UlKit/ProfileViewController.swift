b1391dd00b23b38ba21b414165be7477359b8d32
Ekaterina V <katee.volkova@gmail.com>
2024-02-15 15:37:36 +0100

    feat: create DiscountCardView

--- UlKit/UlKit/ProfileViewController.swift
+++ UlKit/UlKit/ProfileViewController.swift
@@ -3,9 +3,39 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+/// Экран профиля юзера
+final class ProfileViewController: UIViewController {
+    enum Constants {
+        static let screenTitle = "Профиль"
+        static let discountCardPadding = (x: 20.0, y: 25.0)
+    }
+
+    private let discountCardView = DiscountCardView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Профиль"
+        setupView()
+    }
+
+    private func setupView() {
+        title = Constants.screenTitle
+        view.addSubview(discountCardView)
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            discountCardView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: Constants.discountCardPadding.y
+            ),
+            discountCardView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: Constants.discountCardPadding.x
+            ),
+            view.trailingAnchor.constraint(
+                equalTo: discountCardView.trailingAnchor,
+                constant: Constants.discountCardPadding.x
+            )
+        ])
     }
 }
